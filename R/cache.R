fdc31cf8d1ae05e7de2803c79780f759457d7339
guigang xie <gg.xie@bionovogene.com>
2021-11-03 11:33:40 +0800

    add blank source

---
+++ R/cache.R
@@ -0,0 +1,9 @@
+
+#' Get bing cached page
+#' 
+#' @description get cached page from bing server.
+#' @param link one result item from the bing search result list.
+#' 
+const getCachePage as function(link) {
+
+}
